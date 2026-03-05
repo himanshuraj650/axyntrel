7361ae7ec614ab02d57fec5493f1a688b074ec82
Himanshu Raj <himanshuraj650@gmail.com>
2026-03-05 22:48:02 +0530

    fix express wildcard route

--- server/static.ts
+++ server/static.ts
@@ -17,7 +17,7 @@ export function serveStatic(app: Express) {
 
   app.use(express.static(distPath));
 
-  app.use("/*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
-  });
+  app.use("*", (_req, res) => {
+  res.sendFile(path.resolve(distPath, "index.html"));
+});
 }
